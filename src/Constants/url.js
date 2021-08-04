185788ff4a93d7266a813b6c731738fd2ac793c0
recafox <recafox@gmail.com>
2021-08-04 22:16:18 +0800

    eslint setup

--- src/Constants/url.js
+++ src/Constants/url.js
@@ -1,4 +1,4 @@
-const root = "https://afternoon-anchorage-74547.herokuapp.com/";
+const root = "https://afternoon-anchorage-74547.herokuapp.com";
 const urls = {
   imageURL: root + "/image",
   profileURL: root + "/profile",
